beff72c831a3a9d18f63845db20b289bad28cc71
Abreto FU <public@abreto.email>
2020-04-24 13:08:41 +0800

    Make it more robust

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,9 @@ fi
 if [ "$*" = "" ]; then
     echo "Please specify tunnel options and other options via args, eg:"
     echo
-    echo "  # docker run abreto/ssh-tunnel -R 1234:172.17.0.1:4321"
+    echo "  $ docker run abreto/ssh-tunnel -R 1234:172.17.0.1:4321"
     echo
     exit 1
 fi
 
-while true; do
-    ssh -p $SSH_PORT -i $SSH_ID_FILE -gnNT -o "StrictHostKeyChecking=false" -o GatewayPorts=true "$@" $SSH_USER@$SSH_HOST
-done
+exec ssh -p $SSH_PORT -i $SSH_ID_FILE -gnNT -oStrictHostKeyChecking=false -oGatewayPorts=true -oServerAliveInterval=20 -oServerAliveCountMax=3 -oExitOnForwardFailure=yes "$@" $SSH_USER@$SSH_HOST
